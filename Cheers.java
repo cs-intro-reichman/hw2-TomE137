e87487c0f4e483533aeb02b6fb1ab6fa9b7c1bbb
TomE137 <tomeliyahu@gmail.com>
2024-11-21 02:48:03 +0200

    updated all filers with updated solutions

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,25 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String name = args[0].toUpperCase();
+                int cheers = Integer.parseInt(args[1]);
+                String an_list = "AEFHILMNORSX";
+                String output = "";
+                for (int i = 0; i<name.length(); i++) {
+                        output = "Give me a  ";
+                        for (int j = 0; j<an_list.length(); j++) {
+                                if (an_list.indexOf(name.charAt(i)) != -1) {
+                                        output = "Give me an ";
+                                        break;
+                                        
+                                }
+                        }                        
+                        System.out.println(output + name.charAt(i) + ":" + name.charAt(i) + "!");
+                }
+                System.out.println("What does that spell?");
+                while (cheers>0) {
+                        System.out.println(name+"!!!");
+                        cheers -= 1;
+                }
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,19 @@
 public class TimeCalc {
     public static void main(String[] args) {
         // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int added_minutes = Integer.parseInt(args[1]);
+        String output = "";
+        hours = (hours+(minutes+added_minutes)/60)%24;
+        minutes = (minutes+added_minutes)%60;
+        if (hours < 10) {
+            output = "0";
+        }
+        output += hours + ":";
+        if (minutes < 10) {
+            output += "0";
+        }
+        System.out.println(output+minutes);
     }
 }
