233133f560f21de5f91d3e861b65001a31a37e1e
TomE137 <50273189+TomE137@users.noreply.github.com>
2024-11-21 19:50:51 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,24 +1,26 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-                String name = args[0].toUpperCase();
+                String name = args[0];
                 int cheers = Integer.parseInt(args[1]);
                 String an_list = "AEFHILMNORSX";
                 String output = "";
+                String name_uppercase = "";
                 for (int i = 0; i<name.length(); i++) {
                         output = "Give me a  ";
+                        name_uppercase += (char) (name.charAt(i)-32);
                         for (int j = 0; j<an_list.length(); j++) {
-                                if (an_list.indexOf(name.charAt(i)) != -1) {
+                                if (an_list.indexOf((char) (name.charAt(i) - 32)) != -1) {
                                         output = "Give me an ";
                                         break;
                                         
                                 }
                         }                        
-                        System.out.println(output + name.charAt(i) + ": " + name.charAt(i) + "!");
+                        System.out.println(output + (char) (name.charAt(i) - 32) + ": " + (char) (name.charAt(i) - 32) + "!");
                 }
                 System.out.println("What does that spell?");
                 while (cheers>0) {
-                        System.out.println(name+"!!!");
+                        System.out.println(name_uppercase+"!!!");
                         cheers -= 1;
                 }
         }
